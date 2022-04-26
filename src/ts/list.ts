1222f4fa0cc04a313f27f89bf3689dcc25ddbd85
miyazoon <shlngo1019.miya@gmail.com>
2022-04-26 09:21:46 +0900

    リシテアバージョンアップに伴う修正

--- src/ts/list.ts
+++ src/ts/list.ts
@@ -9,7 +9,10 @@ export class List {
             const kinmu = table.rows[i].cells[2 + fitIndex];
             const kyuka = table.rows[i].cells[3 + fitIndex];
             const start = table.rows[i].cells[4 + fitIndex];
-            this.rows.push(new Row(kinmu, kyuka, start));
+
+            const late = table.rows[i].cells[12 + fitIndex];
+            const early = table.rows[i].cells[13 + fitIndex];
+            this.rows.push(new Row(kinmu, kyuka, start, late, early));
         }
     }
 
@@ -18,12 +21,14 @@ export class List {
             const kinmu: string = List.getCellValue(row.kinmu);
             const kyuka: string = List.getCellValue(row.kyuka);
             const start: string = List.getCellValue(row.start).replace(":", "");
+            const late: string = List.getCellValue(row.late);
+            const early: string = List.getCellValue(row.early);
 
             //  休暇取得時の時
             if (kyuka.indexOf("休暇") != -1) {
                 // 勤務区分が「通常勤務」ではない場合は警告.
                 if (kinmu.indexOf("通常勤務") == -1) {
-                    List.alert(row.kinmu,row.kyuka)
+                    List.alert(row.kinmu, row.kyuka)
                 }
             }
 
@@ -31,15 +36,23 @@ export class List {
             if (kinmu.indexOf("午前休") != -1 || kyuka.indexOf("有休（午前）") != -1) {
                 // 勤務区分、休暇区分ともに午前休でなければ警告.
                 if (!(kinmu.indexOf("午前休") != -1 && kyuka.indexOf("有休（午前）") != -1)) {
-                    List.alert(row.kinmu,row.kyuka)
+                    List.alert(row.kinmu, row.kyuka)
                 }
             }
 
             if (start) {
                 // 始業時間と勤務区分が一致しない場合は警告（勤務区分が「午前休」の場合は除く）.
                 if (kinmu.indexOf(start) == -1 && kinmu.indexOf("午前休") == -1) {
-                    List.alert(row.kinmu,row.start)
+                    List.alert(row.kinmu, row.start)
                 }
+            }
+
+            if(late){
+                List.alert(row.late)
+            }
+
+            if(early){
+                List.alert(row.early)
             }
         });
     }
@@ -49,7 +62,7 @@ export class List {
     }
 
     static alert(...cells: HTMLTableCellElement[]): void {
-        cells.forEach(cell =>{
+        cells.forEach(cell => {
             cell.style.backgroundColor = "red";
         })
     }
@@ -59,6 +72,8 @@ class Row {
     private _kinmu: HTMLTableCellElement;
     private _kyuka: HTMLTableCellElement;
     private _start: HTMLTableCellElement;
+    private _late: HTMLTableCellElement;
+    private _early: HTMLTableCellElement;
 
     get kinmu() {
         return this._kinmu;
@@ -69,10 +84,17 @@ class Row {
     get start() {
         return this._start;
     }
-
-    constructor(kinmu: HTMLTableCellElement, kyuka: HTMLTableCellElement, start: HTMLTableCellElement) {
+    get late() {
+        return this._late;
+    }
+    get early() {
+        return this._early;
+    }
+    constructor(kinmu: HTMLTableCellElement, kyuka: HTMLTableCellElement, start: HTMLTableCellElement, late: HTMLTableCellElement, early: HTMLTableCellElement) {
         this._kinmu = kinmu;
         this._kyuka = kyuka;
         this._start = start;
+        this._late = late;
+        this._early = early;
     }
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,35 +5,49 @@ import { List } from "./list";
 if (window.location.hostname === 'job.cloud.lysithea.jp') {
 
     const page: string = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-    switch (page) {
+    switch (page) { //TODO:URLじゃなくてタイトル名に変更する.
         /** 個人 */
-        case 'WC020.jsp': /** 勤休内容登録画面 */
+        case 'WC020': /** 勤休内容登録画面 */
             // 勤怠自動入力セレクトボックスを配置.
             const input: Input = new Input()
             document.getElementsByClassName('funcNav')[0].appendChild(input.div);
             break;
-        
-        case 'WK030.jsp': /** 勤休日次一覧 */
-            // 勤怠リストのチェック(個人)
-            const list: List = new List(false);
+
+        case 'WJ050': /** 勤休日次一覧 */
+            let titleElement: HTMLElement | null = document.getElementById("funcBar-title")
+            let title : string | null;
+            if(titleElement){
+                title = titleElement.textContent
+            }else {
+                title = ""
+            }
+            
+            let list: List;
+            if (title === "社員別勤休内容承認") {
+                // 勤怠リストのチェック(管理者)
+                list = new List(true);
+            } else {
+                // 勤怠リストのチェック(故人)
+                list = new List(false);
+            }
             list.check();
             break;
 
         /** 管理者 */
-        case 'WJ020.jsp': /** 承認待ち勤休内容一覧 */
-        case 'WJ060.jsp': /** 月次承認状況一覧 */
+        case 'WJ020': /** 承認待ち勤休内容一覧 */
+        case 'WJ060': /** 月次承認状況一覧 */
             // 一括承認ボタンを無効化.
             const buttton: HTMLButtonElement = <HTMLButtonElement>document.getElementsByClassName('btn btn-big')[0]
             buttton.disabled = true
             break;
 
-        case "WJ050.jsp":　/** 社員別勤休内容承認 */
+        case "WJ050":　/** 社員別勤休内容承認 */
             // 勤怠リストのチェック(管理者)
             const adminlist: List = new List(true);
             adminlist.check();
             break;
-        
-        case 'ChildInvoker':
+
+        case 'WC200':
             const checker = new Checker()
             const gBody = document.getElementById("gBody");
             if (gBody != null) {
